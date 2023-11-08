1f6238f93ab0a0f4659530600d72fe53b90372fd
Phoebe Chuang <phoebe160723@gmail.com>
2023-11-08 15:15:26 -0500

    Implemented User and UserFactory interfaces for common methods.

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,4 +1,11 @@
 package entity;
+import java.util.ArrayList;
 
 public interface User {
+    String getUsername();
+    String getPassword();
+    Profile getProfile();
+    Playlist getPlaylist();
+    ArrayList<String> getFriendNames();
+    ArrayList<String> getRequests();
 }
--- src/entity/UserFactory.java
+++ src/entity/UserFactory.java
@@ -1,4 +1,7 @@
 package entity;
 
+import java.util.ArrayList;
+
 public interface UserFactory {
+    User create(String username, String password, Profile profile, Playlist playlist, ArrayList<String> friends, ArrayList<String> requests);
 }
