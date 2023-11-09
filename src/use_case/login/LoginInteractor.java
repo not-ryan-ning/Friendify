cb1bbfe72c30764653235f233390728218643893
Phoebe Chuang <phoebe160723@gmail.com>
2023-11-08 21:03:55 -0500

    Implemented LoginInteractor in login use case package.

--- src/use_case/login/LoginInteractor.java
+++ src/use_case/login/LoginInteractor.java
@@ -1,2 +1,33 @@
-package use_case.login;public class LoginInteractor {
+package use_case.login;
+
+import entity.User;
+
+public class LoginInteractor implements LoginInputBoundary {
+    final LoginUserDataAccessInterface userDataAccessObject;
+    final LoginOutputBoundary loginPresenter;
+
+    public LoginInteractor(LoginUserDataAccessInterface userDataAccessInterface,
+                           LoginOutputBoundary loginOutputBoundary) {
+        this.userDataAccessObject = userDataAccessInterface;
+        this.loginPresenter = loginOutputBoundary;
+    }
+
+    @Override
+    public void execute(LoginInputData loginInputData) {
+        String username = loginInputData.getUsername();
+        String password = loginInputData.getPassword();
+        if (!userDataAccessObject.existByName(username)) {
+            loginPresenter.prepareFailView(username + ": Account does not exist.");
+        } else {
+            String savedPassword = userDataAccessObject.get(username).getPassword();
+            if (!password.equals(savedPassword)) {
+                loginPresenter.prepareFailView("Incorrect password for " + username + ".");
+            } else {
+                User user = userDataAccessObject.get(loginInputData.getUsername());
+
+                LoginOutputData loginOutputData = new LoginOutputData(user.getUsername(), false);
+                loginPresenter.prepareSuccessView(loginOutputData);
+            }
+        }
+    }
 }
