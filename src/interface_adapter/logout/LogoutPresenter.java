5319f8c484f356718894a522314706246f5b78d9
Olivia <olivia.w11267@gmail.com>
2023-11-09 17:13:24 -0500

    Implemented LogoutPresenter so that it switches back to login view when user logs out

--- src/interface_adapter/logout/LogoutPresenter.java
+++ src/interface_adapter/logout/LogoutPresenter.java
@@ -1,30 +1,29 @@
 package interface_adapter.logout;
 
 import interface_adapter.ViewManagerModel;
-import interface_adapter.logged_out.LoggedOutViewModel;
 import use_case.logout.LogoutOutputBoundary;
 import use_case.logout.LogoutOutputData;
-import interface_adapter.logged_out.LoggedOutState;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.login.LoginState;
 
 public class LogoutPresenter implements LogoutOutputBoundary {
-    private final LogoutViewModel logoutViewModel;
-    private final LoggedOutViewModel loggedOutViewModel;
+    private final LoginViewModel loginViewModel;
     private ViewManagerModel viewManagerModel;
 
-    public LogoutPresenter(ViewManagerModel viewManagerModel, LogoutViewModel logoutViewModel, LoggedOutViewModel loggedOutViewModel) {
+    public LogoutPresenter(ViewManagerModel viewManagerModel, LoginViewModel loginViewModel) {
         this.viewManagerModel = viewManagerModel;
-        this.logoutViewModel = logoutViewModel;
-        this.loggedOutViewModel = loggedOutViewModel;
+        this.loginViewModel = loginViewModel;
     }
 
     @Override
     public void prepareSuccessView(LogoutOutputData response) {
-        // On success, switch to the logged out view.
-        LoggedOutState loggedOutState = loggedOutViewModel.getState();
-  //    logoutState.setLoggedOutUsers(response.getLoggedoutUser());
-        this.loggedOutViewModel.setState(loggedOutState);
-        this.loggedOutViewModel.firePropertyChanged();
-        this.viewManagerModel.setActiveView(loggedOutViewModel.getViewName());
-        this.viewManagerModel.firePropertyChanged();
+        // On success, switch to log in view as they are now logged out.
+
+        LoginState loginState = loginViewModel.getState();
+        this.loginViewModel.setState(loginState);
+        loginViewModel.firePropertyChanged();
+
+        viewManagerModel.setActiveView(loginViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
     }
 }
