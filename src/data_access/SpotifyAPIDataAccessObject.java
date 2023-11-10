3076ea425c3da29ffb4c939ecdc0b18780dfa317
Phoebe Chuang <phoebe160723@gmail.com>
2023-11-09 23:24:34 -0500

    Implemented setAccessToken method in SpotifyAPIDataAccessObject to obtain access token using the authorization code

--- src/data_access/SpotifyAPIDataAccessObject.java
+++ src/data_access/SpotifyAPIDataAccessObject.java
@@ -2,6 +2,10 @@ package data_access;
 
 import use_case.matching.SpotifyAPIDataAccessInterface;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Base64;
@@ -9,29 +13,71 @@ import java.util.Base64;
 public class SpotifyAPIDataAccessObject implements SpotifyAPIDataAccessInterface {
     private static final String CLIENT_ID = "7af39c08f4c242b89347deca0538bbb1";
     private static final String CLIENT_SECRET = "c85c0140606943c698f2cddaf49b082e";
-    private static final String REDIRECT_URL = "https://github.com/not-ryan-ning/Friendify";
+    private static final String REDIRECT_URI = "https://github.com/not-ryan-ning/Friendify";
+    private String ACCESS_TOKEN;
     private static final String scope = "playlist-read-private playlist-read-collaborative";
     private static final String authUrl = "https://accounts.spotify.com/authorize";
     private static final String tokenUrl = "https://accounts.spotify.com/api/token";
 
-    // Step 1: Redirect the user to the authorization URL
+    // Get the authorization URL
     public String getRequestUserAuthorizationUrl() {
         String state = generateRandomState();
-        return authUrl + "?client_id=" + CLIENT_ID + "&response_type=code&redirect_uri=" + REDIRECT_URL +
+        return authUrl + "?client_id=" + CLIENT_ID + "&response_type=code&redirect_uri=" + REDIRECT_URI +
                 "&scope=" + scope + "&state=" + state;
     }
 
-    public Object[] requestAccessToken(String grantType, String code, String redirectUrl) {
+    // Redirect the user to the authorization URL and extract the authorization code after the user grants permission
+    public String getAuthorizationCode(String authorizationUrl) {
+        String authCode = ""; // Implement this
+        return authCode;
+    }
+
+    // Set access token using the authorization code
+    public void setAccessToken(String authorizationCode) {
+        String tokenRequestBody = "grant_type=authorization_code&code=" + authorizationCode + "&redirect_uri=" + REDIRECT_URI;
+        String authHeader = Base64.getEncoder().encodeToString((CLIENT_ID + ":" + CLIENT_SECRET).getBytes());
+        HttpClient httpClient = HttpClient.newHttpClient();
+
         // Header Parameters:
         // Authorization = 'Authorization': 'Basic' + <base64 encoded client_id:client_secret>
         // Content-Type = 'content-type': 'application/x-www-form-urlencoded'
-        String accessToken = "";
-        String tokenType = "";
-        String scope = "";
-        int expiresIn = 0;
-        String refreshToken = "";
-        return new Object[] {accessToken, tokenType, scope, expiresIn, refreshToken};
+        HttpRequest tokenRequest = HttpRequest.newBuilder()
+                .uri(URI.create(tokenUrl))
+                .header("Authorization", "Basic " + authHeader)
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString(tokenRequestBody))
+                .build();
+
+        try {
+            HttpResponse<String> tokenResponse = httpClient.send(tokenRequest, HttpResponse.BodyHandlers.ofString());
+
+            if (tokenResponse.statusCode() == 200) {
+                // responseBody will have a JSON-formatted response
+                /*
+                {
+                    "access_token": "your_access_token",
+                    "token_type": "Bearer",
+                    "expires_in": 3600,
+                    "refresh_token": "your_refresh_token",
+                    "scope": "playlist-read-private"
+                }
+                 */
+                String responseBody = tokenResponse.body();
+                String accessToken = responseBody.split("\"access_token\":\"")[1].split("\"")[0];
+                ACCESS_TOKEN = accessToken;
+
+                String refreshToken = null;
+                if (responseBody.contains("refresh_token")) {
+                    refreshToken = responseBody.split("\"refresh_token\":\"")[1].split("\"")[0];
+                }
+            } else {
+                System.out.println("Error obtaining access token. Status code: " + tokenResponse.statusCode() + ", Response: " + tokenResponse.body());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
     @Override
     public ArrayList<String> getArtists() {
         ArrayList<String> artists = new ArrayList<String>();
@@ -69,6 +115,7 @@ public class SpotifyAPIDataAccessObject implements SpotifyAPIDataAccessInterface
         return "iu";
     }
 
+    // Generates a random and unique state parameter for the OAuth 2.0 authorization request
     private static String generateRandomState() {
         byte[] bytes = new byte[16];
         new SecureRandom().nextBytes(bytes);
