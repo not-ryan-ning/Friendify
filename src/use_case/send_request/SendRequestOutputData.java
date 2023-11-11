519c30b44f47efdaba3a0e957ddae774d666a832
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-11 16:28:27 -0500

    Implemented the output data class for the send request use case

--- src/use_case/send_request/SendRequestOutputData.java
+++ src/use_case/send_request/SendRequestOutputData.java
@@ -1,4 +1,15 @@
 package use_case.send_request;
 
 public class SendRequestOutputData {
+    private final String receiverUsername;
+    private boolean useCaseFailed;
+
+    public SendRequestOutputData(String receiverUsername, boolean useCaseFailed) {
+        this.receiverUsername = receiverUsername;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public String requestSent(String receiverUsername) {
+        return "Friend Request Sent to" + receiverUsername;
+    }
 }
