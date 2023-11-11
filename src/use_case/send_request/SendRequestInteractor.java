7a3927e91ccc073fa6f313200c3c9930d6ccb1a9
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-11 16:23:47 -0500

    Implemented the interactor the send request use case

--- src/use_case/send_request/SendRequestInteractor.java
+++ src/use_case/send_request/SendRequestInteractor.java
@@ -1,4 +1,31 @@
 package use_case.send_request;
 
-public class SendRequestInteractor {
+import entity.User;
+public class SendRequestInteractor implements SendRequestInputBoundary {
+    final SendRequestUserDataAccessInterface sendRequestUserDAO;
+    final SendRequestOutputBoundary sendRequestPresenter;
+    public SendRequestInteractor(SendRequestUserDataAccessInterface sendRequestUserDataAccessInterface,
+                                 SendRequestOutputBoundary sendRequestOutputBoundary) {
+        this.sendRequestUserDAO = sendRequestUserDataAccessInterface;
+        this.sendRequestPresenter = sendRequestOutputBoundary;
+    }
+    @Override
+    public void execute(SendRequestInputData sendRequestInputData) {
+        String senderUsername = sendRequestInputData.getSenderUsername();
+        String receiverUsername = sendRequestInputData.getReceiverUsername();
+
+        //execute if the sender and receiver are already friends
+        if (sendRequestUserDAO.isFriend(senderUsername, receiverUsername)) {
+            sendRequestPresenter.prepareFailView("Already friends with" + receiverUsername);
+
+            //execute if the sender and receiver are not friends
+        } else {
+            User sender = sendRequestUserDAO.get(senderUsername);
+            User receiver = sendRequestUserDAO.get(receiverUsername);
+            sendRequestUserDAO.sendFriendRequest(sender, receiver);
+
+            SendRequestOutputData sendRequestOutputData = new SendRequestOutputData(receiverUsername, false);
+            sendRequestPresenter.prepareSuccessView(sendRequestOutputData);
+        }
+    }
 }
