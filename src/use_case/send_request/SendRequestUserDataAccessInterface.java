7a3c0bad89a43a854a0682997ea30a7a3082cfca
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-11 16:30:08 -0500

    Implemented the user data access interface for the send request use case

--- src/use_case/send_request/SendRequestUserDataAccessInterface.java
+++ src/use_case/send_request/SendRequestUserDataAccessInterface.java
@@ -1,4 +1,9 @@
 package use_case.send_request;
 
+import entity.User;
+
 public interface SendRequestUserDataAccessInterface {
+    boolean isFriend(String senderUsername, String receiverUsername);
+    void sendFriendRequest(User sender, User receiver);
+    User get(String username);
 }
