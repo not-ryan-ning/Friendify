a4e9b04b05886271b6b6facd117efd15955dd25a
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-13 16:18:13 -0500

    Deleted code SendRequestPresenter, SendRequestState, and SendRequestViewModel as we still need to discuss the views

--- src/interface_adapter/send_request/SendRequestState.java
+++ src/interface_adapter/send_request/SendRequestState.java
@@ -1,30 +1,4 @@
 package interface_adapter.send_request;
 
 public class SendRequestState {
-    private String requestSent = "";
-    private String friendError = null;
-
-    public SendRequestState(SendRequestState copy) {
-        requestSent = copy.requestSent;
-        friendError = copy.friendError;
-    }
-
-    public SendRequestState() {
-    }
-
-    public String getRequestSent() {
-        return requestSent;
-    }
-
-    public void setRequestSent(String requestSent) {
-        this.requestSent = requestSent;
-    }
-
-    public String getFriendError() {
-        return friendError;
-    }
-
-    public void setFriendError(String friendError) {
-        this.friendError = friendError;
-    }
 }
--- src/interface_adapter/send_request/SendRequestViewModel.java
+++ src/interface_adapter/send_request/SendRequestViewModel.java
@@ -1,11 +1,6 @@
 package interface_adapter.send_request;
 
-import interface_adapter.ViewModel;
 
-public class SendRequestViewModel extends ViewModel {
-    private SendRequestState sendRequestState = new SendRequestState();
-    public SendRequestViewModel() {
-        super("send request");
-    }
+public class SendRequestViewModel {
 
 }
