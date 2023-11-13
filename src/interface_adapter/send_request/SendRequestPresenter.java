707d8bfac93a7b68619253f25ea97a2b1cfb3c0a
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-12 19:38:40 -0500

    Initial implementation of the presenter for the send request use case

--- src/interface_adapter/send_request/SendRequestPresenter.java
+++ src/interface_adapter/send_request/SendRequestPresenter.java
@@ -1,4 +1,23 @@
 package interface_adapter.send_request;
 
-public class SendRequestPresenter {
+import interface_adapter.ViewManagerModel;
+import use_case.send_request.SendRequestOutputBoundary;
+import use_case.send_request.SendRequestOutputData;
+
+public class SendRequestPresenter implements SendRequestOutputBoundary {
+    private final SendRequestViewModel sendRequestViewModel;
+    private ViewManagerModel viewManagerModel;
+    public SendRequestPresenter(SendRequestViewModel sendRequestViewModel, ViewManagerModel viewManagerModel) {
+        this.sendRequestViewModel = sendRequestViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+    @Override
+    public void prepareSuccessView(SendRequestOutputData response) {
+
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+
+    }
 }
