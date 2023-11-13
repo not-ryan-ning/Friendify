eacc49c4369361c0b64e6df185d264e2acc23141
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-12 21:26:57 -0500

    Intial implementation of the view model for the send request use case

--- src/interface_adapter/send_request/SendRequestViewModel.java
+++ src/interface_adapter/send_request/SendRequestViewModel.java
@@ -3,4 +3,9 @@ package interface_adapter.send_request;
 import interface_adapter.ViewModel;
 
 public class SendRequestViewModel extends ViewModel {
+    private SendRequestState sendRequestState = new SendRequestState();
+    public SendRequestViewModel() {
+        super("send request");
+    }
+
 }
