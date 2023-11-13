d97595fdad3ab50c332cf0066530eabf3cd6bb06
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-12 20:53:49 -0500

    Updated the prepareSuccessView and prepareFailView methods in SendRequestPresenter

--- src/interface_adapter/send_request/SendRequestPresenter.java
+++ src/interface_adapter/send_request/SendRequestPresenter.java
@@ -7,17 +7,28 @@ import use_case.send_request.SendRequestOutputData;
 public class SendRequestPresenter implements SendRequestOutputBoundary {
     private final SendRequestViewModel sendRequestViewModel;
     private ViewManagerModel viewManagerModel;
+
     public SendRequestPresenter(SendRequestViewModel sendRequestViewModel, ViewManagerModel viewManagerModel) {
         this.sendRequestViewModel = sendRequestViewModel;
         this.viewManagerModel = viewManagerModel;
     }
+
+    // Want pop-up messages for success and fail
     @Override
     public void prepareSuccessView(SendRequestOutputData response) {
+        SendRequestState sendRequestState = sendRequestViewModel.getState();
+        sendRequestState.setRequestSent(response.requestSent());
+        this.sendRequestViewModel.setState(sendRequestState);
+        sendRequestViewModel.firePropertyChanged();
 
+        this.viewManagerModel.setActiveView(sendRequestViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
     }
 
     @Override
     public void prepareFailView(String error) {
-
+        SendRequestState sendRequestState = sendRequestViewModel.getState();
+        sendRequestState.setFriendError(error);
+        sendRequestViewModel.firePropertyChanged();
     }
 }
