908c88702065b9614d8bf33a90436f08b0604329
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-14 00:08:49 -0500

    Implemented the MatchingStrategy interface with relevant methods

--- src/entity/MatchingStrategy.java
+++ src/entity/MatchingStrategy.java
@@ -1,4 +1,11 @@
 package entity;
 
+import java.util.ArrayList;
+
 public interface MatchingStrategy {
+    double getSimilarityScore();
+    // calculates the similarity score of two users
+    void setPlaylistInfo(ArrayList<Object> playlist1Features, ArrayList<Object> playlist2Features);
+    // playlist1Features and playlist2Features are array lists of each track's relevant features,
+    // which includes titles, artists, acousticness, energy, instrumentalness, and valence
 }
