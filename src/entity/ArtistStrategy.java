2d85064146c2ddcda9928932b1cffd866fd58284
Olivia <olivia.w11267@gmail.com>
2023-11-17 16:24:13 -0500

    Implemented the getSimilarityScore method for the ArtistStrategy class for the Matching by Artists strategy.

--- src/entity/ArtistStrategy.java
+++ src/entity/ArtistStrategy.java
@@ -1,4 +1,26 @@
 package entity;
 
+import java.util.ArrayList;
+
 public class ArtistStrategy implements MatchingStrategy {
+
+    private ArrayList playlist1;
+    private ArrayList playlist2;
+
+
+    @Override
+    public double getSimilarityScore() {
+        ArrayList<String> intersection = new ArrayList<>(playlist1);
+        intersection.retainAll(playlist2);
+        // Calculating the percentage of common elements by dividing it by the minimum size of the 2 playlists
+        double percentageInCommon = (double) intersection.size() / Math.min(playlist1.size(), playlist2.size()) * 100.0;
+
+        return percentageInCommon;
+
+    }
+
+    @Override
+    public void setPlaylistInfo(ArrayList<Object> playlist1Features, ArrayList<Object> playlist2Features) {
+
+    }
 }
