d971b38282a2235251f27ec2e572e8955fd0a2ee
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-17 16:11:06 -0500

    Developed the matching by song title strategy, which implements the MatchingStrategy interface

--- src/entity/TitleStrategy.java
+++ src/entity/TitleStrategy.java
@@ -1,4 +1,28 @@
 package entity;
 
-public class TitleStrategy implements MatchingStrategy{
+import java.util.ArrayList;
+
+public class TitleStrategy implements MatchingStrategy {
+    private ArrayList<String> playlist1Titles;
+    private ArrayList<String> playlist2Titles;
+
+    @Override
+    public double getSimilarityScore() {
+
+        // Find the intersection (common song titles) between the two lists
+        ArrayList<String> intersection = new ArrayList<>(playlist1Titles);
+        playlist1Titles.retainAll(playlist2Titles);
+
+        // Calculate the percentage of common song titles
+        double percentageInCommon = (double) intersection.size() /
+                Math.min(playlist1Titles.size(), playlist2Titles.size()) * 100.0;
+
+        return percentageInCommon;
+    }
+
+    @Override
+    public void setPlaylistInfo(ArrayList<Object> playlist1Features, ArrayList<Object> playlist2Features) {
+        this.playlist1Titles = (ArrayList<String>) playlist1Features.get(0);
+        this.playlist2Titles = (ArrayList<String>) playlist2Features.get(0);
+    }
 }
