6be9feae51e63f5c2bd2e38911b24d8f68904c0c
Olivia <olivia.w11267@gmail.com>
2023-11-18 01:38:23 -0500

    Modified DisplayProfileInputData and the relevant methods inside this class to take in a new private instance attribute otherUsername

--- src/use_case/display_profile/DisplayProfileInputData.java
+++ src/use_case/display_profile/DisplayProfileInputData.java
@@ -2,12 +2,18 @@ package use_case.display_profile;
 
 public class DisplayProfileInputData {
     final private String username;
+    final private String otherUsername;
 
-    public DisplayProfileInputData(String username) {
+    public DisplayProfileInputData(String username, String otherUsername) {
         this.username = username;
+        this.otherUsername = otherUsername;
     }
     String getUsername() {
         return username;
+    }
+
+    String getOtherUsername() {
+        return otherUsername;
     }
 
 }
