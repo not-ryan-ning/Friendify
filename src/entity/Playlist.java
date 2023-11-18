76ceb49df782cbf2e027de6c37c22a029cb14bf6
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-18 01:55:33 -0500

    Implemented Playlist interface entity with abstract getter methods

--- src/entity/Playlist.java
+++ src/entity/Playlist.java
@@ -4,5 +4,11 @@ import java.util.ArrayList;
 
 public interface Playlist {
     public String getPlaylistId();
+    public ArrayList<String> getArtists();
+    public ArrayList<String> getTitles();
+    public double getAcousticness();
+    public double getEnergy();
+    public double getInstrumentalness();
+    public double getValence();
     public ArrayList<String> getTopThreeArtists();
 }
