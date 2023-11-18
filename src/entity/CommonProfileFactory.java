e9474fe537fd2c2d181f2d54daecc36934e71b25
Ryan <ryanning15243@gmail.com>
2023-11-18 06:54:49 -0500

    Implemented Common Profile Factory Class

--- src/entity/CommonProfileFactory.java
+++ src/entity/CommonProfileFactory.java
@@ -1,2 +1,12 @@
-package entity;public class CommonProfileFactory {
+package entity;
+
+import java.util.ArrayList;
+
+public class CommonProfileFactory implements ProfileFactory{
+    @Override
+    public Profile create(String bio,
+                          ArrayList<String> topThreeArtists,
+                          String spotifyHandle) {
+        return new CommonProfile(bio, topThreeArtists, spotifyHandle);
+    }
 }
