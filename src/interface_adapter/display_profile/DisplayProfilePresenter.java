0aca318446be2baa840fcffaa6b5a131c4f02e1c
Olivia <olivia.w11267@gmail.com>
2023-11-18 01:36:09 -0500

    Began implementation for DisplayProfilePresenter

--- src/interface_adapter/display_profile/DisplayProfilePresenter.java
+++ src/interface_adapter/display_profile/DisplayProfilePresenter.java
@@ -1,4 +1,24 @@
 package interface_adapter.display_profile;
 
-public class DisplayProfilePresenter {
+import use_case.display_profile.DisplayProfileInputData;
+import use_case.display_profile.DisplayProfileOutputBoundary;
+import use_case.display_profile.DisplayProfileOutputData;
+
+public class DisplayProfilePresenter implements DisplayProfileOutputBoundary {
+    private final DisplayProfileViewModel displayProfileViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public DisplayProfilePresenter(DisplayProfileViewModel displayProfileViewModel, ViewManagerModel viewManagerModel) {
+        this.displayProfileViewModel = displayProfileViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+
+    @Override
+    public void prepareSuccessView(DisplayProfileOutputData user) {
+        // switch to what?
+    }
+    public void prepareFailView(String error) {
+
+    }
 }
