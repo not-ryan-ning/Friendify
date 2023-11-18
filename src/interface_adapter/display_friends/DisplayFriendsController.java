384009f9857ded0f7d18e130ba106a01db3972d8
Phoebe Chuang <phoebe160723@gmail.com>
2023-11-17 23:09:25 -0500

    Implemented displayFriendsController

--- src/interface_adapter/display_friends/DisplayFriendsController.java
+++ src/interface_adapter/display_friends/DisplayFriendsController.java
@@ -1,4 +1,18 @@
 package interface_adapter.display_friends;
 
+import use_case.display_friends.DisplayFriendsInputBoundary;
+import use_case.display_friends.DisplayFriendsInputData;
+
 public class DisplayFriendsController {
+
+    final DisplayFriendsInputBoundary displayFriendsInteractor;
+    public DisplayFriendsController(DisplayFriendsInputBoundary displayFriendsInteractor) {
+        this.displayFriendsInteractor = displayFriendsInteractor;
+    }
+
+    public void execute(String username) {
+        DisplayFriendsInputData displayFriendsInputData = new DisplayFriendsInputData(username);
+
+        displayFriendsInteractor.execute(displayFriendsInputData);
+    }
 }
