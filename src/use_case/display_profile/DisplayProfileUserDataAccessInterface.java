0a6879c4c35687ffa76d772c18eb122428669750
Olivia <olivia.w11267@gmail.com>
2023-11-17 23:58:52 -0500

    Added getFriends method in DisplayProfileUserDataAccessInterface.java to return a user's list of friends

--- src/use_case/display_profile/DisplayProfileUserDataAccessInterface.java
+++ src/use_case/display_profile/DisplayProfileUserDataAccessInterface.java
@@ -1,4 +1,11 @@
 package use_case.display_profile;
 
+import entity.User;
+
+import java.util.ArrayList;
+
 public interface DisplayProfileUserDataAccessInterface {
+    User get(String username);
+
+    ArrayList<String> getFriends(String username);
 }
