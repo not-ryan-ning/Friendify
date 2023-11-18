227d0bf31e972f91832fa969ace9693b4eef6c78
Olivia <olivia.w11267@gmail.com>
2023-11-17 22:47:26 -0500

    Implemented OutputData for Display Profile

--- src/use_case/display_profile/DisplayProfileInteractor.java
+++ src/use_case/display_profile/DisplayProfileInteractor.java
@@ -1,4 +1,17 @@
 package use_case.display_profile;
 
-public class DisplayProfileInteractor {
+public class DisplayProfileInteractor implements DisplayProfileInputBoundary {
+    final DisplayProfileUserDataAccessInterface displayProfileUserDataAccessObject;
+    final DisplayProfileOutputBoundary displayProfilePresenter;
+
+    public DisplayProfileInteractor(DisplayProfileUserDataAccessInterface displayProfileUserDataAccessInterface, DisplayProfileOutputBoundary displayProfileOutputBoundary) {
+        this.displayProfileUserDataAccessObject = displayProfileUserDataAccessInterface;
+        this.displayProfilePresenter = displayProfileOutputBoundary;
+    }
+
+    @Override
+    public void execute() {
+        DisplayProfileOutputData displayProfileOutputData = new DisplayProfileOutputData();
+        displayProfilePresenter.prepareSuccessView(displayProfileOutputData);
+    }
 }
--- src/use_case/display_profile/DisplayProfileOutputBoundary.java
+++ src/use_case/display_profile/DisplayProfileOutputBoundary.java
@@ -1,4 +1,4 @@
 package use_case.display_profile;
 
-public class DisplayProfileOutputBoundary {
+public interface DisplayProfileOutputBoundary {
 }
