d60ec9ef8af597388e3df23ca78fb981954394dd
Olivia <olivia.w11267@gmail.com>
2023-11-18 01:35:33 -0500

    Added a prepare fail view in OutputBoundary for when two users aren't friends.

--- src/use_case/display_profile/DisplayProfileOutputBoundary.java
+++ src/use_case/display_profile/DisplayProfileOutputBoundary.java
@@ -2,4 +2,5 @@ package use_case.display_profile;
 
 public interface DisplayProfileOutputBoundary {
     void prepareSuccessView(DisplayProfileOutputData user);
+    void prepareFailView(String error);
 }
