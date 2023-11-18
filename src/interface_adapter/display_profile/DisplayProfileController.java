591dc1909d797bb3ee7b95297ab515f6c88845ae
Olivia <olivia.w11267@gmail.com>
2023-11-17 23:43:41 -0500

    Implemented DisplayProfileController for display profile use case

--- src/interface_adapter/display_profile/DisplayProfileController.java
+++ src/interface_adapter/display_profile/DisplayProfileController.java
@@ -1,4 +1,15 @@
 package interface_adapter.display_profile;
 
+import use_case.display_profile.DisplayProfileInputBoundary;
+
 public class DisplayProfileController {
+    final DisplayProfileInputBoundary displayProfileUseCaseInteractor;
+
+    public DisplayProfileController(DisplayProfileInputBoundary displayProfileUseCaseInteractor) {
+        this.displayProfileUseCaseInteractor = displayProfileUseCaseInteractor;
+    }
+
+    public void execute() {
+        displayProfileUseCaseInteractor.execute();
+    }
 }
