0203319998b62c7a6803115fbc2c39005ca0e308
Ryan <ryanning15243@gmail.com>
2023-11-17 20:19:28 -0500

    Implemented Signup Input Boundary Interface

---
+++ src/use_case/signup/SignupOutputData.java
@@ -0,0 +1,21 @@
+package use_case.signup;
+
+public class SignupOutputData {
+
+    private final String username;
+    private boolean useCaseSuccess;
+
+    public SignupOutputData(String username, boolean useCaseFailed {
+        this.username = username;
+        this.useCaseSuccess = useCaseSuccess;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean getUseCaseSuccess(){
+        return useCaseSuccess;
+    }
+
+}
