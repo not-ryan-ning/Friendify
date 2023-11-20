09e2aae5b0d353caa8e83330dcb32fdd7096f34d
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-20 00:55:35 -0500

    Changed the isFriend() method to isRequested(), and added javadoc

--- src/use_case/send_request/SendRequestUserDataAccessInterface.java
+++ src/use_case/send_request/SendRequestUserDataAccessInterface.java
@@ -3,7 +3,21 @@ package use_case.send_request;
 import entity.User;
 
 public interface SendRequestUserDataAccessInterface {
-    boolean isFriend(String senderUsername, String receiverUsername);
+
+    /**
+     * Checks for the sender's username in the receiver's list of friend requests, and returns
+     * whether the sender has previously requested the receiver.
+     * @param sender
+     * @param receiver
+     * @return true if previously requested (false otherwise)
+     */
+    boolean isRequested(User sender, User receiver);
+
+    /**
+     * Sends a friend request by adding the sender's username into the receiver's list of friend requests.
+     * @param sender
+     * @param receiver
+     */
     void sendFriendRequest(User sender, User receiver);
     User get(String username);
 }
