f7b87eb1671eb2c5a8ff8b63b2e6c68f5aa69428
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-19 22:22:10 -0500

    Implemented the interactor class for the matching use case

--- src/use_case/matching/MatchingInteractor.java
+++ src/use_case/matching/MatchingInteractor.java
@@ -1,4 +1,26 @@
 package use_case.matching;
 
-public class MatchingInteractor {
+import entity.MatchingStrategy;
+import entity.Playlist;
+import entity.User;
+
+import java.util.ArrayList;
+
+public class MatchingInteractor implements MatchingInputBoundary {
+    final MatchingUserDataAccessInterface matchingUserDAO;
+    final MatchingOutputBoundary matchingPresenter;
+    public MatchingInteractor(MatchingUserDataAccessInterface matchingUserDataAccessInterface,
+                                 MatchingOutputBoundary matchingOutputBoundary) {
+        this.matchingUserDAO = matchingUserDataAccessInterface;
+        this.matchingPresenter = matchingOutputBoundary;
+    }
+
+    @Override
+    public void execute(MatchingInputData matchingInputData) {
+        User currentUser = matchingUserDAO.get(matchingInputData.getCurrentUsername());
+        ArrayList<String> matches = matchingUserDAO.match(currentUser);
+
+        MatchingOutputData matchingOutputData = new MatchingOutputData(matches);
+        matchingPresenter.prepareSuccessView(matchingOutputData);
+    }
 }
