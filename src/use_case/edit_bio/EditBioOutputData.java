e2f1825a02fa3874dbdcbef1695f4191ee9c43c7
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-20 17:37:59 -0500

    Implemented the output data class for the edit bio use case

--- src/use_case/edit_bio/EditBioOutputData.java
+++ src/use_case/edit_bio/EditBioOutputData.java
@@ -1,2 +1,12 @@
-package use_case.edit_bio;public class EditBioOutputData {
+package use_case.edit_bio;
+
+public class EditBioOutputData {
+    private String newBio;
+
+    public EditBioOutputData(String newBio) {
+        this.newBio = newBio;
+    }
+    public String getNewBio() {
+        return newBio;
+    }
 }
