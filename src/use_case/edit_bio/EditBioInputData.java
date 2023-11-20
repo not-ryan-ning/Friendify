c5758dc707fe4767871499d5b383dd4e57e7e608
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-20 17:35:40 -0500

    Implemented the input data class for the edit bio use case

--- src/use_case/edit_bio/EditBioInputData.java
+++ src/use_case/edit_bio/EditBioInputData.java
@@ -1,2 +1,12 @@
-package use_case.edit_bio;public class EditBioInputData {
+package use_case.edit_bio;
+
+public class EditBioInputData {
+    private String newBio;
+
+    public EditBioInputData(String newBio) {
+        this.newBio = newBio;
+    }
+    String getNewBio() {
+        return newBio;
+    }
 }
