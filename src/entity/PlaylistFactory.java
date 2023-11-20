be5dbaa180f6fe77cacdf84a24b096b83e695646
Phoebe Chuang <phoebe160723@gmail.com>
2023-11-19 21:45:59 -0500

    Finished implementing CommonPlaylist, CommonPlaylistFactory, and PlaylistFactory entities

--- src/entity/PlaylistFactory.java
+++ src/entity/PlaylistFactory.java
@@ -1,5 +1,10 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public interface PlaylistFactory {
-    Playlist create()
+    Playlist create(String playlistId, HashMap<String, Integer> titles, HashMap<String, Integer> artists,
+                    HashMap<String, Integer> genres, double acousticness, double energy,
+                    double instrumentalness, double valence, ArrayList<String> topThreeArtists);
 }
