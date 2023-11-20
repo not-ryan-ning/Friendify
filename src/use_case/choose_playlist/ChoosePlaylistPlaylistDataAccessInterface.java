cc4a35748591b5d8b255a11d8cd6ca062888f8df
Phoebe Chuang <phoebe160723@gmail.com>
2023-11-19 21:48:13 -0500

    Modified the parameters of ChoosePlaylist use case's data access interfaces

--- src/use_case/choose_playlist/ChoosePlaylistPlaylistDataAccessInterface.java
+++ src/use_case/choose_playlist/ChoosePlaylistPlaylistDataAccessInterface.java
@@ -1,10 +1,10 @@
 package use_case.choose_playlist;
 
+import entity.Playlist;
+
 import java.util.HashMap;
 
 public interface ChoosePlaylistPlaylistDataAccessInterface {
-    void addPlaylist(String playlistId, HashMap<String, Integer> titles,
-                     HashMap<String, Integer> artists, HashMap<String, Integer> genres,
-                     double acousticness, double energy, double instrumentalness, double valence);
-    String gerPlaylistName(String playlistId);
+    void storePlaylist(Playlist playlist);
+    String getPlaylistName(String playlistId);
 }
--- src/use_case/choose_playlist/ChoosePlaylistUserDataAccessInterface.java
+++ src/use_case/choose_playlist/ChoosePlaylistUserDataAccessInterface.java
@@ -1,9 +1,10 @@
 package use_case.choose_playlist;
 
+import entity.Playlist;
 import entity.User;
 
 import java.util.HashMap;
 
 public interface ChoosePlaylistUserDataAccessInterface {
-    void editPlaylist(String username, String playlistId, HashMap<String, Integer> artists);
+    void editPlaylist(String username, Playlist playlist);
 }
