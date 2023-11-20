6badcd03eecc4de09474f285060480926a64f4b2
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-20 17:01:22 -0500

    Modified the setter methods in the Profile interface entity to take in arguments

--- src/entity/Profile.java
+++ src/entity/Profile.java
@@ -6,7 +6,7 @@ public interface Profile {
     public String getBio();
     public ArrayList<String> getTopThreeArtists();
     public String getSpotifyHandle();
-    public void setBio();
-    public void setSpotifyHandle();
+    public void setBio(String newBio);
+    public void setSpotifyHandle(String newSpotifyHandle);
     public void setTopThreeArtists();
 }
---
+++ src/use_case/edit_bio/EditBioInputData.java
@@ -0,0 +1,2 @@
+package use_case.edit_bio;public class EditBioInputData {
+}
