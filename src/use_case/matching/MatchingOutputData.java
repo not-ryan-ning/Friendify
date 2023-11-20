328139ac1e2b4097315b0299e5eac92f6dca937b
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-19 22:23:40 -0500

    Implemented the output data class for the matching use case

--- src/use_case/matching/MatchingOutputData.java
+++ src/use_case/matching/MatchingOutputData.java
@@ -1,4 +1,14 @@
 package use_case.matching;
 
+import java.util.ArrayList;
+
 public class MatchingOutputData {
+    private final ArrayList<String> matches;
+
+    public MatchingOutputData(ArrayList<String> matches) {
+        this.matches = matches;
+    }
+    public ArrayList<String> getMatches() {
+        return matches;
+    }
 }
