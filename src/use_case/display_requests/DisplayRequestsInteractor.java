e85ac95497a23bc9c5f12c1979d0b0e73eb047fe
Olivia <olivia.w11267@gmail.com>
2023-11-20 16:02:07 -0500

    Implemented the DisplayRequestsInteractor for display requests

--- src/use_case/display_requests/DisplayRequestsInteractor.java
+++ src/use_case/display_requests/DisplayRequestsInteractor.java
@@ -1,4 +1,25 @@
 package use_case.display_requests;
 
-public class DisplayRequestsInteractor {
+import entity.User;
+public class DisplayRequestsInteractor implements DisplayRequestsInputBoundary {
+    final DisplayRequestsUserDataAccessInterface userDataAccessObject;
+    final DisplayRequestsOutputBoundary displayRequestsPresenter;
+
+    public DisplayRequestsInteractor(DisplayRequestsUserDataAccessInterface userDataAccessInterface,
+                                    DisplayRequestsOutputBoundary displayFriendsOutputBoundary) {
+        this.userDataAccessObject = userDataAccessInterface;
+        this.displayRequestsPresenter = displayFriendsOutputBoundary;
+    }
+
+    @Override
+    public void execute(DisplayRequestsInputData displayRequestsInputData) {
+        String username = displayRequestsInputData.getUsername();
+
+        User user = userDataAccessObject.get(username);
+
+        DisplayRequestsOutputData displayFriendsOutputData = new DisplayRequestsOutputData(user.getFriendNames());
+        displayRequestsPresenter.prepareSuccessView(displayFriendsOutputData);
+    }
+}
+
 }
