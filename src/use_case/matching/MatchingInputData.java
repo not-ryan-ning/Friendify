061375c73a39d303ecea7d131749dad3bd939ae8
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-19 22:21:32 -0500

    Implemented the input data class for the matching use case

--- src/use_case/matching/MatchingInputData.java
+++ src/use_case/matching/MatchingInputData.java
@@ -1,4 +1,12 @@
 package use_case.matching;
 
 public class MatchingInputData {
+    final private String currentUsername;
+
+    public MatchingInputData(String currentUsername) {
+        this.currentUsername = currentUsername;
+    }
+    String getCurrentUsername() {
+        return currentUsername;
+    }
 }
