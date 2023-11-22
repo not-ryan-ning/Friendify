8e4f1a88dd321f5d239f250d4f733afa0c4ddc32
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-22 01:38:16 -0500

    Modified the calculations for similarity score to follow the Jaccard similarity measure

--- src/entity/TitleStrategy.java
+++ src/entity/TitleStrategy.java
@@ -1,22 +1,30 @@
 package entity;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class TitleStrategy implements MatchingStrategy {
 
     @Override
     public double getSimilarityScore(Playlist playlist1, Playlist playlist2) {
+        // Get the lists of song titles from the input Playlist Objects, and create sets to get unique elements
+        Set<String> titles1 = new HashSet<>(playlist1.getTitles());
+        Set<String> titles2 = new HashSet<>(playlist2.getTitles());
 
-        // Find the intersection (common song titles) between the two lists
-        ArrayList<String> intersection = new ArrayList<>(playlist1.getTitles());
-        playlist1.getTitles().retainAll(playlist2.getTitles());
+        // Find the intersection (common song titles) between the two sets
+        Set<String> intersection = new HashSet<>(titles1);
+        intersection.retainAll(titles2);
 
-        // Calculate the percentage of common song titles
-        double percentageInCommon = (double) intersection.size() /
-                Math.min(playlist1.getTitles().size(), playlist2.getTitles().size()) * 100.0;
-        // Denominator is the minimum of the two lists
-        // Allows % in common to reach 100%
+        // Find the union of the two sets
+        Set<String> union = new HashSet<>(titles1);
+        union.addAll(titles2);
 
-        return percentageInCommon;
+        // Calculate Jaccard similarity score
+        double similarityScore = 0.0;
+        if (!union.isEmpty()) {
+            similarityScore = (double) intersection.size() / union.size();
+        }
+        return similarityScore;
     }
 }
