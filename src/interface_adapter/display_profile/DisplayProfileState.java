f49bbe2e9cab46b1a07ed785300de687226480ed
Olivia <olivia.w11267@gmail.com>
2023-11-25 17:57:16 -0500

    Implemented DisplayProfileState

--- src/interface_adapter/display_profile/DisplayProfileState.java
+++ src/interface_adapter/display_profile/DisplayProfileState.java
@@ -1,4 +1,27 @@
 package interface_adapter.display_profile;
-
+import entity.Profile;
 public class DisplayProfileState {
+    private Profile commonProfile;
+    private Profile friendProfile;
+
+    public DisplayProfileState(DisplayProfileState copy) {
+        this.commonProfile = copy.commonProfile;
+        this.friendProfile = copy.friendProfile;
+    }
+    public DisplayProfileState() {}
+
+    public Profile getCommonProfile() {
+        return this.commonProfile;
+    }
+    public Profile getFriendProfile() {
+        return this.friendProfile;
+    }
+
+    public void setCommonProfile(Profile commmonProfile) {
+        this.commonProfile = commonProfile;
+    }
+
+    public void setFriendProfile(Profile friendProfile) {
+        this.commonProfile = commonProfile;
+    }
 }
