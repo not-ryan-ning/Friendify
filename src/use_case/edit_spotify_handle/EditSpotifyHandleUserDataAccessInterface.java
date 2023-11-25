7d1783d677d5eaf8f536eb23656d773f13289a49
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-25 12:21:12 -0500

    Removed the import statement for the User entity as it is no longer needed

--- src/use_case/edit_spotify_handle/EditSpotifyHandleUserDataAccessInterface.java
+++ src/use_case/edit_spotify_handle/EditSpotifyHandleUserDataAccessInterface.java
@@ -1,7 +1,5 @@
 package use_case.edit_spotify_handle;
 
-import entity.User;
-
 public interface EditSpotifyHandleUserDataAccessInterface {
     void editFile(String Username, String column, String newValue);
 }
