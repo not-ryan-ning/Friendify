c2846484991d73c2300419da73ed779ec219bb3d
Olivia <olivia.w11267@gmail.com>
2023-11-25 17:38:05 -0500

    Implemented DisplayProfileViewModel and added ViewManager

--- src/interface_adapter/display_profile/DisplayProfileViewModel.java
+++ src/interface_adapter/display_profile/DisplayProfileViewModel.java
@@ -1,4 +1,32 @@
 package interface_adapter.display_profile;
 
-public class DisplayProfileViewModel {
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class DisplayProfileViewModel extends ViewModel {
+
+    private DisplayProfileState state = new DisplayProfileState();
+
+    public DisplayProfileViewModel() { super("display profile");
+    }
+
+
+    public void setState(DisplayProfileState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+    public DisplayProfileState getState() {
+        return state;
+    }
 }
