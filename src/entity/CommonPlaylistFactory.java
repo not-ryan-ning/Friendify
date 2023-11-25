f1df1aa98d82d9188a768d7e6d051051ab1d583b
Phoebe Chuang <phoebe160723@gmail.com>
2023-11-25 14:43:27 -0500

    Modified the type of titles attribute from hash map to arraylist

--- src/entity/CommonPlaylistFactory.java
+++ src/entity/CommonPlaylistFactory.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 
 public class CommonPlaylistFactory implements PlaylistFactory {
     @Override
-    public CommonPlaylist create(String playlistId, HashMap<String, Integer> titles, HashMap<String, Integer> artists,
+    public CommonPlaylist create(String playlistId, ArrayList<String> titles, HashMap<String, Integer> artists,
                                  HashMap<String, Integer> genres, double acousticness, double energy,
                                  double instrumentalness, double valence, ArrayList<String> topThreeArtists) {
         return new CommonPlaylist(playlistId, titles, artists, genres, acousticness, energy, instrumentalness,
