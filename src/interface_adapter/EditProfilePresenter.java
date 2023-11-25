0fc90dfc2c9136fc0e05e784d556084f76e476b3
Olivia <olivia.w11267@gmail.com>
2023-11-25 04:49:11 -0500

    Implemented prepareSuccessView in EditProfilePresenter so that it changes back to the Profile View after a user is done editing their profile.

--- src/interface_adapter/EditProfilePresenter.java
+++ src/interface_adapter/EditProfilePresenter.java
@@ -1,4 +1,19 @@
 package interface_adapter;
 
-public class EditProfilePresenter {
+public class EditProfilePresenter implements EditProfileOutputBoundary{
+
+    @Override
+    public void prepareSuccessView(EditProfileOutputData response) {
+        EditProfileState editProfileState = editProfileViewModel.getState();
+
+        this.editProfileViewModel.setState(editProfileState);
+        this.editProfileViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setActiveView(editProfileViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+
+        // We now switch back to the Profile View after user is done editing their profile
+        viewManagerModel.setActiveView(profileViewModel.getViewName()); // assuming there is a "profile view"
+        viewManagerModel.firePropertyChanged();
+    }
 }
