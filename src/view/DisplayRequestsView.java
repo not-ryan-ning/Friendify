87991fb638b0463e428aaaa7d84c396dd893349f
Olivia <olivia.w11267@gmail.com>
2023-11-25 13:32:24 -0500

    Implemented DisplayRequestsView so that a user sees a list of their requests with corresponding buttons to view that request's profile. Some methods are not fully implemented yet.

--- src/view/DisplayRequestsView.java
+++ src/view/DisplayRequestsView.java
@@ -1,27 +1,68 @@
 package view;
 
 import interface_adapter.display_requests.DisplayRequestsController;
+import interface_adapter.display_requests.DisplayRequestsState;
 import interface_adapter.display_requests.DisplayRequestsViewModel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
 
-public class DisplayRequestsView {
+import entity.User;
+
+public class DisplayRequestsView extends JPanel implements ActionListener, PropertyChangeListener {
+
     public final String viewname = "Display Requests";
     private final DisplayRequestsViewModel displayRequestsViewModel;
     private final DisplayRequestsController displayRequestsController;
+    private final DisplayProfileController displayProfileController;
 
-    //how are we going to map each view profile button to a user?
-    public DisplayRequestsView(DisplayRequestsViewModel displayRequestsViewModel, DisplayRequestsController displayRequestsController) {
+    public DisplayRequestsView(DisplayRequestsViewModel displayRequestsViewModel, DisplayRequestsController displayRequestsController,
+                               DisplayProfileController displayProfileController) {
         this.displayRequestsViewModel = displayRequestsViewModel;
         this.displayRequestsController = displayRequestsController;
+        this.displayProfileController = displayProfileController;
 
         JLabel title = new JLabel(DisplayRequestsViewModel.TITLE_LABEL);
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        // buttons go here
+        JPanel buttons = new JPanel();
 
+        // Assuming that there is a user object being passed around
+        ArrayList<String> requests = user.getRequests;
+        for (String request : requests) {
+            JButton view = new JButton("view " + request + "'s profile");
+            buttons.add(view);
 
+            view.addActionListener(
+                    new ActionListener() {
+                        public void actionPerformed(ActionEvent evt) {
+                            if (evt.getSource().equals(view)) {
+                                DisplayRequestsState currentState = displayRequestsViewModel.getState();
+
+                                displayProfileController.execute();
+                            }
+                        }
+                    }
+            );
+            this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+            this.add(title);
+            this.add(buttons);
+        }
     }
 
+    @Override
+    public void actionPerformed(ActionEvent evt) {
+        System.out.println("Click " + evt.getActionCommand());
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
