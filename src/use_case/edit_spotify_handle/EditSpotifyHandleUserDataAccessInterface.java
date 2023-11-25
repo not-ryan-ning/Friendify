f9358019f600c8ac05c050523bb449764c4f6bfa
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-25 12:18:18 -0500

    Removed the User get method in EditSpotifyHandleUserDataAccessInterface

--- src/use_case/edit_spotify_handle/EditSpotifyHandleUserDataAccessInterface.java
+++ src/use_case/edit_spotify_handle/EditSpotifyHandleUserDataAccessInterface.java
@@ -3,6 +3,5 @@ package use_case.edit_spotify_handle;
 import entity.User;
 
 public interface EditSpotifyHandleUserDataAccessInterface {
-    User get(String Username);
     void editFile(String Username, String column, String newValue);
 }
