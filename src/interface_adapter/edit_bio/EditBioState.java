f913ac8c64289786fa642b4a11d52b65e28cc182
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-26 01:01:00 -0500

    Implemented the EditBioState class

--- src/interface_adapter/edit_bio/EditBioState.java
+++ src/interface_adapter/edit_bio/EditBioState.java
@@ -1,4 +1,16 @@
 package interface_adapter.edit_bio;
 
 public class EditBioState {
+    private String bio;
+
+    public EditBioState(EditBioState copy) {
+        this.bio = copy.bio;
+    }
+    public EditBioState(){}
+    public String getBio() {
+        return bio;
+    }
+    public void setBIo(String bio) {
+        this.bio = bio;
+    }
 }
