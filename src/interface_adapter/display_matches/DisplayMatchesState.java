fa8dd059c58132d6cde4c7521120f65ca891f5e5
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-26 00:10:00 -0500

    Added a receiverUsername and requestError attribute to the DisplayMatchesStateClass, as well as their getter/setter methods

--- src/interface_adapter/display_matches/DisplayMatchesState.java
+++ src/interface_adapter/display_matches/DisplayMatchesState.java
@@ -4,8 +4,13 @@ import java.util.HashMap;
 
 public class DisplayMatchesState {
     private HashMap<String, Double> matches;
+    private String receiverUsername;
+    private String requestError = null;
+
     public DisplayMatchesState(DisplayMatchesState copy) {
         this.matches = copy.matches;
+        this.receiverUsername = copy.receiverUsername;
+        this.requestError = copy.requestError;
     }
     public DisplayMatchesState() {}
 
@@ -14,5 +19,17 @@ public class DisplayMatchesState {
     }
     public void setMatches(HashMap<String, Double> matches) {
         this.matches = matches;
+    }
+    public String getReceiverUsername() {
+        return receiverUsername;
+    }
+    public void setReceiverUsername(String recieverUsername) {
+        this.receiverUsername = recieverUsername;
+    }
+    public String getRequestError() {
+        return requestError;
+    }
+    public void setRequestError(String requestError) {
+        this.requestError = requestError;
     }
 }
