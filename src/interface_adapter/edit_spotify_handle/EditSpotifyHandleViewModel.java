453211e5849f3b71d169de2f7fb33ea46c217938
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-27 10:51:18 -0500

    Implemented the EditSpotifyHandleViewModel class

--- src/interface_adapter/edit_spotify_handle/EditSpotifyHandleViewModel.java
+++ src/interface_adapter/edit_spotify_handle/EditSpotifyHandleViewModel.java
@@ -1,4 +1,29 @@
 package interface_adapter.edit_spotify_handle;
 
-public class EditSpotifyHandleViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class EditSpotifyHandleViewModel extends ViewModel {
+    public static final String SPOTIFY_HANDLE_LABEL = "Type Spotify Handle";
+    public static final String SAVE_SPOTIFY_HANDLE_LABEL = "Save Spotify Handle";
+    private EditSpotifyHandleState state = new EditSpotifyHandleState();
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public EditSpotifyHandleState getState() {
+        return state;
+    }
+    public EditSpotifyHandleViewModel() {
+        super("edit spotify handle");
+    }
+    public void setState(EditSpotifyHandleState state) {
+        this.state = state;
+    }
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 }
