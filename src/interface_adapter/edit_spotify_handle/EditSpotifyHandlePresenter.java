34350cc8e5782714e8dd28b9c9e976543576cc2d
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-27 11:08:56 -0500

    Implemented the EditSpotifyHandlePresenter class

--- src/interface_adapter/edit_spotify_handle/EditSpotifyHandlePresenter.java
+++ src/interface_adapter/edit_spotify_handle/EditSpotifyHandlePresenter.java
@@ -1,4 +1,28 @@
 package interface_adapter.edit_spotify_handle;
 
-public class EditSpotifyHandlePresenter {
+import interface_adapter.ViewManagerModel;
+import use_case.edit_spotify_handle.EditSpotifyHandleOutputBoundary;
+import use_case.edit_spotify_handle.EditSpotifyHandleOutputData;
+
+public class EditSpotifyHandlePresenter implements EditSpotifyHandleOutputBoundary {
+    private final EditSpotifyHandleViewModel editSpotifyHandleViewModel;
+
+    private ViewManagerModel viewManagerModel;
+
+    public EditSpotifyHandlePresenter(ViewManagerModel viewManagerModel,
+                                      EditSpotifyHandleViewModel editSpotifyHandleViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.editSpotifyHandleViewModel = editSpotifyHandleViewModel;
+    }
+    @Override
+    public void prepareSuccessView(EditSpotifyHandleOutputData response) {
+
+        EditSpotifyHandleState clearState = editSpotifyHandleViewModel.getState();
+        clearState.setSpotifyHandle(response.getNewSpotifyHandle());
+        this.editSpotifyHandleViewModel.setState(clearState);
+        editSpotifyHandleViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setActiveView(editSpotifyHandleViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
 }
