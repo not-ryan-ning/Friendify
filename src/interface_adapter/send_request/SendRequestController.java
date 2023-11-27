112287c1cfe65b880bbd1af4bb463d04f5cdc0b2
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-27 16:15:17 -0500

    Files that were accidentally deleted

--- src/interface_adapter/send_request/SendRequestController.java
+++ src/interface_adapter/send_request/SendRequestController.java
@@ -1,4 +1,17 @@
 package interface_adapter.send_request;
 
+import use_case.send_request.SendRequestInputBoundary;
+import use_case.send_request.SendRequestInputData;
+
 public class SendRequestController {
+    final SendRequestInputBoundary sendRequestInteractor;
+
+    public SendRequestController(SendRequestInputBoundary sendRequestInteractor) {
+        this.sendRequestInteractor = sendRequestInteractor;
+    }
+
+    public void execute(String senderUsername, String receiverUsername) {
+        SendRequestInputData sendRequestInputData = new SendRequestInputData(receiverUsername);
+        sendRequestInteractor.execute(senderUsername, sendRequestInputData);
+    }
 }
--- src/interface_adapter/send_request/SendRequestPresenter.java
+++ src/interface_adapter/send_request/SendRequestPresenter.java
@@ -1,4 +1,35 @@
 package interface_adapter.send_request;
 
-public class SendRequestPresenter {
+import interface_adapter.ViewManagerModel;
+import use_case.send_request.SendRequestOutputBoundary;
+import use_case.send_request.SendRequestOutputData;
+
+public class SendRequestPresenter implements SendRequestOutputBoundary {
+
+    private final SendRequestViewModel sendRequestViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public SendRequestPresenter(SendRequestViewModel sendRequestViewModel,
+                                ViewManagerModel viewManagerModel) {
+        this.sendRequestViewModel = sendRequestViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    @Override
+    public void prepareSuccessView(SendRequestOutputData response) {
+        SendRequestState sendRequestState = sendRequestViewModel.getState();
+        sendRequestState.setReceiverUsername(response.getReceiverUsername());
+        this.sendRequestViewModel.setState(sendRequestState);
+        sendRequestViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setActiveView(sendRequestViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        SendRequestState sendRequestState = sendRequestViewModel.getState();
+        sendRequestState.setRequestError(error);
+        sendRequestViewModel.firePropertyChanged();
+    }
 }
