ad8f32f2241f697270f9be47a6a44f854249ea78
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-27 13:03:13 -0500

    Implemented the SendRequestViewModel class

--- src/interface_adapter/send_request/SendRequestViewModel.java
+++ src/interface_adapter/send_request/SendRequestViewModel.java
@@ -1,6 +1,23 @@
 package interface_adapter.send_request;
 
+import interface_adapter.ViewModel;
 
-public class SendRequestViewModel {
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
+public class SendRequestViewModel extends ViewModel {
+    private SendRequestState sendRequestState = new SendRequestState();
+
+    public SendRequestViewModel() { super("send request"); }
+
+    public void setState(SendRequestState state) { this.sendRequestState = state; }
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("clearState", null, this.sendRequestState);
+    }
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+    public SendRequestState getState() { return sendRequestState; }
 }
