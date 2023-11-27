ddd969aa16d6c435fd290fc9fa2c4d18c0a3098d
Phoebe Chuang <phoebe160723@gmail.com>
2023-11-27 13:13:49 -0500

    Implemented authorize interface adapter files

--- src/interface_adapter/authorize/AuthorizeViewModel.java
+++ src/interface_adapter/authorize/AuthorizeViewModel.java
@@ -1,2 +1,28 @@
-package interface_adapter.authorize;public class AuthorizeViewModel {
+package interface_adapter.authorize;
+
+import interface_adapter.ViewModel;
+import interface_adapter.login.LoginState;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class AuthorizeViewModel extends ViewModel {
+    private AuthorizeState state = new AuthorizeState();
+    public AuthorizeViewModel() { super("authorize"); }
+    public void setState(AuthorizeState state) {
+        this.state = state;
+    }
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("authorizeState", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public AuthorizeState getState() {
+        return state;
+    }
 }
