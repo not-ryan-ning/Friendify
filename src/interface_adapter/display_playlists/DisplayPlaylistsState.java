fa81a9b3c40fdb80c1d7b9ceb57ca185852daa13
Phoebe Chuang <phoebe160723@gmail.com>
2023-11-27 02:02:01 -0500

    Implemented DisplayPlaylist's controller, presenter, state, and view model

--- src/interface_adapter/display_playlists/DisplayPlaylistsState.java
+++ src/interface_adapter/display_playlists/DisplayPlaylistsState.java
@@ -1,4 +1,25 @@
 package interface_adapter.display_playlists;
 
+import java.util.HashMap;
+
 public class DisplayPlaylistsState {
+    private HashMap<String, String> playlistIdName;
+    private String accessToken;
+
+    public DisplayPlaylistsState(DisplayPlaylistsState copy) {
+        playlistIdName = copy.playlistIdName;
+    }
+
+    public DisplayPlaylistsState() {}
+
+    public HashMap<String, String> getPlaylistIdName() { return playlistIdName; }
+
+    public String getAccessToken() { return accessToken; }
+    public void setPlaylistIdName(HashMap<String, String> playlistIdName) {
+        this.playlistIdName = playlistIdName;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
 }
