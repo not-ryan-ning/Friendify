310efacc96e020efefda35c22e40a3a7fa372597
Olivia <olivia.w11267@gmail.com>
2023-11-29 18:21:30 -0500

    Implemented state and controller

--- src/interface_adapter/goBack/GoBackController.java
+++ src/interface_adapter/goBack/GoBackController.java
@@ -1,4 +1,15 @@
 package interface_adapter.goBack;
 
+import use_case.goBack.GoBackInputBoundary;
+
 public class GoBackController {
+    final GoBackInputBoundary goBackUseCaseInteractor;
+
+    public GoBackController(GoBackInputBoundary logoutUseCaseInteractor) {
+        this.goBackUseCaseInteractor = logoutUseCaseInteractor;
+    }
+
+    public void execute() {
+        goBackUseCaseInteractor.execute();
+    }
 }
