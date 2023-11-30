acba4629b7cfe8e98c6cf3de0bcab4c80d9a24e6
Phoebe Chuang <phoebe160723@gmail.com>
2023-11-29 22:08:23 -0500

    Implemented display friends view files

--- src/view/DisplayRequestsView.java
+++ src/view/DisplayRequestsView.java
@@ -1,5 +1,6 @@
 package view;
 
+import interface_adapter.display_friends.DisplayFriendsState;
 import interface_adapter.display_requests.DisplayRequestsController;
 import interface_adapter.display_requests.DisplayRequestsState;
 import interface_adapter.display_requests.DisplayRequestsViewModel;
@@ -13,57 +14,92 @@ import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 
 import entity.User;
+import interface_adapter.edit_bio.EditBioState;
+import interface_adapter.logged_in.LoggedInViewModel;
 
 public class DisplayRequestsView extends JPanel implements ActionListener, PropertyChangeListener {
 
-    public final String viewname = "Display Requests";
-    private final DisplayRequestsViewModel displayRequestsViewModel;
+    public final String viewname = "display requests";
     private final DisplayRequestsController displayRequestsController;
+    private final DisplayRequestsViewModel displayRequestsViewModel;
     private final DisplayProfileController displayProfileController;
+    private final DisplayProfileViewModel displayProfileViewModel;
+    private final AcceptRequestsController acceptRequestsController;
+    private final AcceptRequestsViewModel acceptRequestsViewModel;
     private final LoggedInViewModel loggedInViewModel;
 
-    public DisplayRequestsView(DisplayRequestsViewModel displayRequestsViewModel, DisplayRequestsController displayRequestsController,
-                               DisplayProfileController displayProfileController, LoggedInViewModel loggedInViewModel) {
-        this.displayRequestsViewModel = displayRequestsViewModel;
+    public DisplayRequestsView(DisplayRequestsViewModel displayRequestsViewModel,
+                               DisplayRequestsController displayRequestsController,
+                               DisplayProfileViewModel displayProfileViewModel,
+                               DisplayProfileController displayProfileController,
+                               AcceptRequestsController acceptRequestsController,
+                               AcceptRequestsViewModel acceptRequestsViewModel,
+                               LoggedInViewModel loggedInViewModel) {
         this.displayRequestsController = displayRequestsController;
+        this.displayRequestsViewModel = displayRequestsViewModel;
         this.displayProfileController = displayProfileController;
+        this.displayProfileViewModel = displayProfileViewModel;
+        this.acceptRequestsController = acceptRequestsController;
+        this.acceptRequestsViewModel = acceptRequestsViewModel;
         this.loggedInViewModel = loggedInViewModel;
+
+        displayRequestsViewModel.addPropertyChangeListener(this);
+        displayProfileViewModel.addPropertyChangeListener(this);
+        acceptRequestsViewModel.addPropertyChangeListener(this);
 
         JLabel title = new JLabel(DisplayRequestsViewModel.TITLE_LABEL);
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         JPanel buttons = new JPanel();
 
-
-        // Adding view profile and accept request buttons for every request
         DisplayRequestsState currentState = displayRequestsViewModel.getState();
         ArrayList<String> requests = currentState.getRequests();
+
         for (String request : requests) {
             JLabel requestUsername = new JLabel(request);
             this.add(requestUsername);
 
-            JButton view = new JButton(DisplayRequestsViewModel.VIEW_BUTTON_LABEL);
-            buttons.add(view);
+            JButton viewProfile = new JButton(DisplayProfileViewModel.VIEW_BUTTON_LABEL);
+            // Associate each view profile button with the corresponding request username
+            viewProfile.putClientProperty("userString", request);
+            buttons.add(viewProfile);
 
-            JButton accept = new JButton(DisplayRequestsViewModel.ACCEPT_BUTTON_LABEL);
-            buttons.add(accept);
+            JButton acceptRequest = new JButton(AcceptRequestsViewModel.ACCEPT_BUTTON_LABEL);
+            // Associate each accept button with the corresponding request username
+            acceptRequest.putClientProperty("userString", request);
+            buttons.add(acceptRequest);
 
-            view.addActionListener(
+            viewProfile.addActionListener(
                     new ActionListener() {
                         public void actionPerformed(ActionEvent evt) {
-                            if (evt.getSource().equals(view)) {
-                                displayProfileController.execute();
+                            if (evt.getSource().equals(viewProfile)) {
+                                DisplayRequestsState currentState = displayRequestsViewModel.getState();
+                                // Retrieve the associated request name
+                                String associatedString = (String) viewProfile.getClientProperty("userString");
+                                currentState.setRequestName(associatedString);
+
+                                displayProfileController.execute(
+                                        currentState.getUsername(),
+                                        currentState.getRequestName()
+                                );
                             }
                         }
                     }
             );
 
-            accept.addActionListener(
+            acceptRequest.addActionListener(
                     new ActionListener() {
                         public void actionPerformed(ActionEvent evt) {
-                            if (evt.getSource().equals(accept)) {
+                            if (evt.getSource().equals(acceptRequest)) {
+                                AcceptRequestsState currentState = acceptRequestViewModel.getState();
+                                // Retrieve the associated request name
+                                String associatedString = (String) acceptRequest.getClientProperty("userString");
+                                currentState.setRequestName(associatedString);
 
-                                acceptRequestController.execute(); // Ryan's use case
+                                acceptRequestController.execute(
+                                        currentState.getUsername(),
+                                        currentState.getRequestName()
+                                ); // Ryan's use case
                             }
                         }
                     }
@@ -76,13 +112,10 @@ public class DisplayRequestsView extends JPanel implements ActionListener, Prope
     }
 
     @Override
-    public void actionPerformed(ActionEvent evt) {
-        JOptionPane.showConfirmDialog(this, "You have accepted this request.");
-    }
-
-    @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        //does not display anything for either state...
-
+        if (evt.getPropertyName().equals("acceptRequestsState")) {
+            AcceptRequestsState acceptRequestsState = (AcceptRequestsState) evt.getNewValue();
+            JOptionPane.showMessageDialog(this, "You have accepted a request.");
+        }
     }
 }
