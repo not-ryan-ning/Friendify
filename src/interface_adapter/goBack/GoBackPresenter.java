cea79ddb84558a141b560934582713d5945ffd8d
Olivia <olivia.w11267@gmail.com>
2023-11-29 22:24:25 -0500

    Implementing go back presenter

--- src/interface_adapter/goBack/GoBackPresenter.java
+++ src/interface_adapter/goBack/GoBackPresenter.java
@@ -3,8 +3,15 @@ package interface_adapter.goBack;
 import use_case.goBack.GoBackOutputBoundary;
 
 public class GoBackPresenter implements GoBackOutputBoundary {
+    private final GoBackViewModel goBackViewModel;
+    private ViewManagerModel viewManagerModel;
+    public GoBackPresenter(GoBackViewModel goBackViewModel, ViewManagerModel viewManagerModel) {
+        this.goBackViewModel = goBackViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
     @Override
     public void prepareSuccessView() {
-
+        // On success, switch to the previous view.
     }
 }
