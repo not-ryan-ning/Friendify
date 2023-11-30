d9991afcbf01d4fdf93a4e01f0003bdc0d7bea4a
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-11-30 17:54:39 -0500

    Added setter methods to the Profile entity

--- src/entity/Profile.java
+++ src/entity/Profile.java
@@ -6,4 +6,7 @@ public interface Profile {
     String getBio();
     ArrayList<String> getTopThreeArtists();
     String getSpotifyHandle();
+    void setBio(String bio);
+    void setSpotifyHandle(String spotifyHandle);
+    void setTopThreeArtists(ArrayList<String> topThreeArtists);
 }
