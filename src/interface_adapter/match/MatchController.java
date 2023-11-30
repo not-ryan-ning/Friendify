2ebf841ab1610bcd720b8a1d8bbe0d262256e2ff
Phoebe Chuang <phoebe160723@gmail.com>
2023-11-30 00:15:08 -0500

    Modified DisplayRequests and Logout interface adapters

--- src/interface_adapter/match/MatchController.java
+++ src/interface_adapter/match/MatchController.java
@@ -7,12 +7,8 @@ import use_case.match.MatchInputBoundary;
 public class MatchController {
     final MatchInputBoundary matchInteractor;
 
-    final LoggedInState loggedInState;
-
-    public MatchController(MatchInputBoundary matchInputBoundary,
-                           LoggedInState loggedInState) {
+    public MatchController(MatchInputBoundary matchInputBoundary) {
         this.matchInteractor = matchInputBoundary;
-        this.loggedInState = loggedInState;
     }
 
     public void execute(String currentUsername) {
