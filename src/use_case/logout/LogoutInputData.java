84ce8b407139fd78915ebce6bb6494bf550a77ca
Phoebe Chuang <phoebe160723@gmail.com>
2023-11-30 00:14:06 -0500

    Removed LoggedInUseCaseFactory and implemented LoginUseCaseFactory

--- src/use_case/logout/LogoutInputData.java
+++
@@ -1,5 +0,0 @@
-package use_case.logout;
-
-public class LogoutInputData {
-    // Doesn't take any input data from user.
-}
--- src/use_case/logout/LogoutUserDataAccessInterface.java
+++
@@ -1,7 +0,0 @@
-package use_case.logout;
-
-import entity.User;
-
-public interface LogoutUserDataAccessInterface {
-    User get(String username);
-}
