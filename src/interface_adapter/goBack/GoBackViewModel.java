d89abd770b2b62d6d977dd2521c0c7f6866c757d
Olivia <olivia.w11267@gmail.com>
2023-11-29 22:19:00 -0500

    Implemented GoBackViewModel

--- src/interface_adapter/goBack/GoBackViewModel.java
+++ src/interface_adapter/goBack/GoBackViewModel.java
@@ -1,4 +1,30 @@
 package interface_adapter.goBack;
 
-public class GoBackViewModel {
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class GoBackViewModel extends ViewModel {
+    private GoBackState state = new GoBackState();
+    public GoBackViewModel() {
+        super("go back");
+    }
+
+    public void setState(GoBackState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("goBackState", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public GoBackState getState() {
+        return state;
+    }
 }
+
