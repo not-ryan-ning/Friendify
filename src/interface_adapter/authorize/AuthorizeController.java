c2f47c39ad311b682b0c3351db3b03a31b07a296
Olivia <olivia.w11267@gmail.com>
2023-12-01 23:49:46 -0500

    Began javadoc for AuthorizeController

--- src/interface_adapter/authorize/AuthorizeController.java
+++ src/interface_adapter/authorize/AuthorizeController.java
@@ -5,6 +5,11 @@ import use_case.authorize.AuthorizeInputBoundary;
 public class AuthorizeController {
     final AuthorizeInputBoundary authorizeInteractor;
 
+    /**
+     * The AuthorizeController class is responsible for handling authorization-related operations.
+     *
+     * @param authorizeInteractor
+     */
     public AuthorizeController(AuthorizeInputBoundary authorizeInteractor) {
         this.authorizeInteractor = authorizeInteractor;
     }
