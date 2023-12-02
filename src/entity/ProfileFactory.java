7069fef4dd52dbf04fed03381f7cea14e892c2d1
Olivia <olivia.w11267@gmail.com>
2023-12-01 22:44:05 -0500

    Added Javadoc for ProfileFactory

--- src/entity/ProfileFactory.java
+++ src/entity/ProfileFactory.java
@@ -2,7 +2,20 @@ package entity;
 
 import java.util.ArrayList;
 
+/**
+ *  The ProfileFactory interface defines a template for classes that implement
+ *  the creation of user profiles.
+ */
 public interface ProfileFactory {
+
+    /**
+     * Creates a Profile instance with the specified initial attributes.
+     *
+     * @param bio             A String representing the user's bio.
+     * @param topThreeArtists An ArrayList containing the names of the user's top three artists.
+     * @param spotifyHandle   A String representing the user's Spotify handle.
+     * @return A Profile object with the specified initial attributes.
+     */
     Profile create(String bio, ArrayList<String> topThreeArtists, String spotifyHandle);
 
 }
