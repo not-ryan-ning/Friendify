e8e5c468bd7351e08d7c8e5ac75f3d27cee13b6d
Olivia <olivia.w11267@gmail.com>
2023-12-01 22:46:50 -0500

    Added Javadoc for User

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,6 +1,10 @@
 package entity;
 import java.util.ArrayList;
 
+/**
+ * The User interface represents a user in the system and defines methods
+ * for accessing various attributes and functionalities related to the user.
+ */
 public interface User {
     String getUsername();
     String getPassword();
