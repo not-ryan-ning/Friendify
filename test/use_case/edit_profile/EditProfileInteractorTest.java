e059eaf5d6fa18bda50a8a9373e7550ce28174d4
Olivia <olivia.w11267@gmail.com>
2023-12-02 18:26:31 -0500

    Added edit profile interactor test

---
+++ test/use_case/edit_profile/EditProfileInteractorTest.java
@@ -0,0 +1,19 @@
+package use_case.edit_profile;
+
+import interface_adapter.ViewManagerModel;
+import org.junit.Test;
+
+import javax.swing.text.View;
+
+import static org.junit.Assert.*;
+
+public class EditProfileInteractorTest {
+    private EditProfileInteractor editProfileInteractor;
+    private ViewManagerModel viewManagerModel;
+    @Test
+    public void testExecute() {
+        editProfileInteractor.execute();
+        assertEquals("Edit Profile", viewManagerModel.getActiveView());
+    }
+
+}
