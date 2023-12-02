c241b1be962cf269d27210b27a1c057088b6dfbb
Olivia <olivia.w11267@gmail.com>
2023-12-01 22:36:21 -0500

    Added Javadoc for MatchingStrategy

--- src/entity/MatchingStrategy.java
+++ src/entity/MatchingStrategy.java
@@ -2,7 +2,19 @@ package entity;
 
 import java.util.ArrayList;
 
+/**
+ * The MatchingStrategy interface provides a template for classes, with
+ * a method for calculating the similarity score between two playlists.
+ */
+
 public interface MatchingStrategy {
+    /**
+     *
+     * @param playlist1 The first Playlist object for comparison.
+     * @param playlist2 The second Playlist object for comparison.
+     * @return A double representing the similarity score between the two playlists. The score depends
+     * on the specific MatchingStrategy implementation.
+     */
     double getSimilarityScore(Playlist playlist1, Playlist playlist2);
     // calculates the similarity score of two users' playlists
 }
