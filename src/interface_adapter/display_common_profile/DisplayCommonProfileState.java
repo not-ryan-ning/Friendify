9513831e21d032ddaf23a439ddd7211c966fc7a2
Olivia <olivia.w11267@gmail.com>
2023-12-01 23:04:34 -0500

    Added Javadoc for DisplayCommonProfileState

--- src/interface_adapter/display_common_profile/DisplayCommonProfileState.java
+++ src/interface_adapter/display_common_profile/DisplayCommonProfileState.java
@@ -8,22 +8,45 @@ public class DisplayCommonProfileState {
     private Profile commonProfile;
     private String username;
 
+    /**
+     * Constructs a new DisplayCommonProfileState instance by copying the state from another instance.
+     *
+     * @param copy The DisplayCommonProfileState to copy from.
+     */
     public DisplayCommonProfileState(DisplayCommonProfileState copy) {
         this.commonProfile = copy.commonProfile;
         this.username = copy.username;
 
     }
 
+    /**
+     * Constructs a new DisplayCommonProfileState instance
+     */
     public DisplayCommonProfileState() {}
 
+    /**
+     * Gets the common profile associated with this state.
+     *
+     * @return The common profile.
+     */
     public Profile getCommonProfile() {
         return commonProfile;
     }
 
+    /**
+     * Sets the common profile for this state.
+     *
+     * @param commonProfile The common profile to set.
+     */
     public void setCommonProfile(Profile commonProfile) {
         this.commonProfile = commonProfile;
     }
 
+    /**
+     * Gets the username associated with this state.
+     *
+     * @return The username.
+     */
     public String getUsername() {
         return username;
     }
