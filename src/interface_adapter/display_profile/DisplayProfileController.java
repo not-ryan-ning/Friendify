5c9a3d4ad5dc8aa990e07d3b12b35ddc46f990bf
Olivia <olivia.w11267@gmail.com>
2023-12-01 23:16:29 -0500

    Added Javadoc for DisplayProfileController.

--- src/interface_adapter/display_profile/DisplayProfileController.java
+++ src/interface_adapter/display_profile/DisplayProfileController.java
@@ -6,10 +6,21 @@ import use_case.display_profile.DisplayProfileInputData;
 public class DisplayProfileController {
     final DisplayProfileInputBoundary displayProfileUseCaseInteractor;
 
+    /**
+     * Constructs an instance of DisplayProfileController using DisplayProfileInputBoundary.
+     *
+     * @param displayProfileUseCaseInteractor An implementation of the DisplayProfileInputBoundary interface.
+     */
     public DisplayProfileController(DisplayProfileInputBoundary displayProfileUseCaseInteractor) {
         this.displayProfileUseCaseInteractor = displayProfileUseCaseInteractor;
     }
 
+    /**
+     * Initiates the display profile functionality by calling the execute method.
+     *
+     * @param username      The username of the current user who wants to view a profile.
+     * @param otherUsername The username of the profile to be displayed.
+     */
     public void execute(String username, String otherUsername) {
         DisplayProfileInputData displayProfileInputData = new DisplayProfileInputData(username, otherUsername);
         displayProfileUseCaseInteractor.execute(displayProfileInputData);
