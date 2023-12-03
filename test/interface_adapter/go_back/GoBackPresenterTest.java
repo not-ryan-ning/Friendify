5f5db6eac0cc3afeffdd5f3c0617e4fb6c8a31eb
Olivia <olivia.w11267@gmail.com>
2023-12-03 14:47:41 -0500

    Made changes to presenter test

--- test/interface_adapter/go_back/GoBackPresenterTest.java
+++ test/interface_adapter/go_back/GoBackPresenterTest.java
@@ -5,6 +5,7 @@ import interface_adapter.ViewManagerModel;
 import interface_adapter.logged_in.LoggedInViewModel;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import view.DisplayCommonProfileView;
 import view.LoggedInView;
 
 import javax.swing.*;
@@ -13,9 +14,9 @@ import java.awt.*;
 import static org.junit.Assert.*;
 
 public class GoBackPresenterTest {
-    private ViewManagerModel viewManagerModel;
-    private GoBackViewModel goBackViewModel;
-    private LoggedInViewModel loggedInViewModel;
+    final private ViewManagerModel viewManagerModel = new ViewManagerModel();
+    final private GoBackViewModel goBackViewModel = new GoBackViewModel();
+    final private LoggedInViewModel loggedInViewModel = new LoggedInViewModel();
     @BeforeEach
     public JButton getButton() {
         JFrame app = null;
@@ -32,12 +33,13 @@ public class GoBackPresenterTest {
         Component cp = ((JRootPane) root).getContentPane();
         JPanel jp = (JPanel) cp;
         JPanel jp2 = (JPanel) jp.getComponent(0);
-        LoggedInView sv = (LoggedInView) jp2.getComponent(0);
+
+        // Choosing to test the go back button found on the Display Common Profile View
+        DisplayCommonProfileView sv = (DisplayCommonProfileView) jp2.getComponent(0);
         JPanel buttons = (JPanel) sv.getComponent(0);
-        return (JButton) buttons.getComponent(0); //should be the go back button
+        return (JButton) buttons.getComponent(0); //The go back button is the only button in this view
     }
     /**
-     *
      * Test GoBack's prepareSuccessView to ensure it switches the views back to the loggedInView on click
      */
     @Test
