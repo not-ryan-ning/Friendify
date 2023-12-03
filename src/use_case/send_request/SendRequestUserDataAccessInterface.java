5dbf4282d285caf3de651efc9385f3ba2407ccde
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-12-03 13:24:06 -0500

    Edited javaDoc for send request use case files

--- src/use_case/send_request/SendRequestUserDataAccessInterface.java
+++ src/use_case/send_request/SendRequestUserDataAccessInterface.java
@@ -6,14 +6,14 @@ public interface SendRequestUserDataAccessInterface {
 
     /**
      * Returns whether the sender has previously requested the receiver.
-     * @param sender
-     * @param receiver
+     * @param sender The user who wants to send the friend request
+     * @param receiver The user who is being requested
      * @return True if previously requested (False otherwise)
      */
     boolean isRequested(User sender, User receiver);
 
     /**
-     * Sends a friend request by adding the sender's username into the receiver's list of friend requests.
+     * Sends a friend request from one user to another
      * @param sender The user who is sending the friend request
      * @param receiver The user who is receiving the friend request
      */
