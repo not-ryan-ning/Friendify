b1988ca431a6c475006ca95e157cd0d15ad625bd
Ryan <ryanning15243@gmail.com>
2023-12-03 11:58:55 -0500

    Implemented basic methods and 'initializers' for AcceptRequestViewModel Class

--- src/interface_adapter/accept_request/AcceptRequestViewModel.java
+++ src/interface_adapter/accept_request/AcceptRequestViewModel.java
@@ -1,4 +1,11 @@
 package interface_adapter.accept_request;
 
-public class AcceptRequestViewModel {
+public class AcceptRequestViewModel extends ViewModel {
+    public static final String TITLE_LABEL = "Display Requests View";
+    public static final String VIEW_BUTTON_LABEL = "View Profile";
+    public static final String ACCEPT_BUTTON_LABEL = "Accept Request";
+    private AcceptRequestState state = new AcceptRequestState();
+    public AcceptRequestViewModel() {
+        super("show requests");
+    }
 }
