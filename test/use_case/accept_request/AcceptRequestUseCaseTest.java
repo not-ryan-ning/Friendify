9d82bb7887cd38e5cdba70331ee0c5c07fbecade
Ryan <ryanning15243@gmail.com>
2023-12-03 12:09:59 -0500

    Implemented testInteractorLogic method in AcceptRequestUseCaseTest Class

--- test/use_case/accept_request/AcceptRequestUseCaseTest.java
+++ test/use_case/accept_request/AcceptRequestUseCaseTest.java
@@ -7,6 +7,10 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class AcceptRequestUseCaseTest {
     private UserFactory userFactory;
@@ -35,6 +39,43 @@ public class AcceptRequestUseCaseTest {
     public void testInteractorCreatedProperly() {
         AcceptRequestInteractor interactor = new AcceptRequestInteractor(mockAcceptRequestFileUserDA0,
                 mockAcceptRequestPresenter);
+
+    }
+
+    public void testInteractorLogic() {
+        // user2 has requested user1's friendship
+        // user1 accepts user2 as a friend
+        // Initialize 2 arrays with a single user in it, user1 or user2
+        ArrayList<String> user1List = new ArrayList<>();
+        user1List.add("John");
+        ArrayList<String> user2List = new ArrayList<>();
+        user2List.add("Alabaster");
+
+        User user1 = userFactory.create("John", "password", emptyProfile, emptyPlaylist,
+                new ArrayList<>(), new ArrayList<>(List.of("Alabaster")));
+        User user2 = userFactory.create("Alabaster", "password", emptyProfile, emptyPlaylist,
+                new ArrayList<>(), new ArrayList<>());
+        ((MockFileUserDataAccessObject) mockAcceptRequestFileUserDA0).putUser(user1);
+        ((MockFileUserDataAccessObject) mockAcceptRequestFileUserDA0).putUser(user2);
+
+        AcceptRequestInteractor interactor = new AcceptRequestInteractor(mockAcceptRequestFileUserDA0,
+                mockAcceptRequestPresenter);
+        AcceptRequestInputData acceptRequestInputData = new AcceptRequestInputData(user1.getUsername(), user2.getUsername());
+
+        interactor.execute(acceptRequestInputData);
+
+        // Test that user1 has added user2 to their friend list
+        assertEquals(user2List,
+                ((MockFileUserDataAccessObject) mockAcceptRequestFileUserDA0).getAccounts().get("John").getFriends());
+
+        // Test that user2 has been removed from user1's request list
+        assertTrue(((MockFileUserDataAccessObject)
+                mockAcceptRequestFileUserDA0).getAccounts().get("John").getRequests().isEmpty());
+
+        // Test that user1 has been added to user2's friend list
+        assertEquals(user1List,
+                ((MockFileUserDataAccessObject)
+                        mockAcceptRequestFileUserDA0).getAccounts().get("Alabaster").getFriends());
 
     }
 }
