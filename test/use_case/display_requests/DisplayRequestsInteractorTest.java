20e4b4296110e72783a4807aca92b6b33aa97aa7
Olivia <olivia.w11267@gmail.com>
2023-12-02 19:15:06 -0500

    made changes

--- test/use_case/display_requests/DisplayRequestsInteractorTest.java
+++ test/use_case/display_requests/DisplayRequestsInteractorTest.java
@@ -5,8 +5,10 @@ import entity.CommonPlaylist;
 import entity.CommonProfile;
 import entity.CommonUser;
 import entity.User;
+import interface_adapter.ViewManagerModel;
 import interface_adapter.display_requests.DisplayRequestsState;
 import org.junit.Test;
+import use_case.send_request.SendRequestInputData;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -16,25 +18,17 @@ import java.util.HashMap;
 import static org.junit.Assert.*;
 
 public class DisplayRequestsInteractorTest {
-
-    private DisplayRequestsUserDataAccessInterface displayRequestsUserDataAccessInterface;
     private DisplayRequestsInteractor displayRequestsInteractor;
+    private ViewManagerModel viewManagerModel;
 
-    /**
-     * Testing that DisplayRequestsInteractor correctly
-     */
     @Test
     public void testExecute() {
-        User user = new CommonUser("user1", "password1", new CommonProfile("Hi", new ArrayList<>(),
-                "handle1"), new CommonPlaylist("", new ArrayList<>(), new HashMap<>(), new HashMap<>(),
-                0, 0,0,0, new ArrayList<>()), new ArrayList<>(Arrays.asList("user2")), new ArrayList<>());
+//        User user = new CommonUser("user1", "password1", new CommonProfile("Hi", new ArrayList<>(),
+//                "handle1"), new CommonPlaylist("", new ArrayList<>(), new HashMap<>(), new HashMap<>(),
+//                0, 0,0,0, new ArrayList<>()), new ArrayList<>(Arrays.asList("user2")), new ArrayList<>());
 
-        DisplayRequestsState state = new DisplayRequestsState();
-        state.setRequests(new ArrayList<>(Arrays.asList("user1", "user2")));
-
-        displayRequestsInteractor.execute(user.getUsername());
-
-        assertEquals(user.getRequests(), new ArrayList<>(Arrays.asList("user1", "user2")));
+        displayRequestsInteractor.execute("user1");
+        assertEquals("show requests", viewManagerModel.getActiveView());
     }
 
 }
