6ab2bc12d7b61114a81462ccdea5cda399e91baa
Ryan <ryanning15243@gmail.com>
2023-12-03 12:10:38 -0500

    Implemented MockAcceptRequestPresenter  Class for testing purposes

--- test/use_case/accept_request/MockAcceptRequestPresenter.java
+++ test/use_case/accept_request/MockAcceptRequestPresenter.java
@@ -1,4 +1,7 @@
 package use_case.accept_request;
 
-public class MockAcceptRequestPresenter {
+public class MockAcceptRequestPresenter implements AcceptRequestOutputBoundary {
+    @Override
+    public void prepareSuccessView(AcceptRequestOutputData acceptRequestOutputData) {
+    }
 }
