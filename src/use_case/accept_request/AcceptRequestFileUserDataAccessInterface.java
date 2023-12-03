ca17dcf311f2d1cbbdbc9bbce5b361d68e6c3170
Ryan <ryanning15243@gmail.com>
2023-12-03 11:40:38 -0500

    Implemented empty methods in AcceptRequestFileUserDataAccessInterface interface.

--- src/use_case/accept_request/AcceptRequestFileUserDataAccessInterface.java
+++ src/use_case/accept_request/AcceptRequestFileUserDataAccessInterface.java
@@ -1,4 +1,9 @@
 package use_case.accept_request;
 
+import entity.User;
+
 public interface AcceptRequestFileUserDataAccessInterface {
+    void updateUserInformation(User user);
+
+    User get(String currentUsername);
 }
