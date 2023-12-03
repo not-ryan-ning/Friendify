656c2f83df9e9d3b5fb43dae9ff57f174452ff44
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2023-12-03 02:06:18 -0500

    Added javaDoc for the relevant files in use_case.send_request

--- src/use_case/send_request/SendRequestOutputBoundary.java
+++ src/use_case/send_request/SendRequestOutputBoundary.java
@@ -1,6 +1,15 @@
 package use_case.send_request;
 
 public interface SendRequestOutputBoundary {
+    /**
+     * Trigger the successful view of the send request use case
+     * @param user
+     */
     void prepareSuccessView(SendRequestOutputData user);
+
+    /** Trigger the fail view of the send request use case
+     *
+     * @param error The error from the send request use case
+     */
     void prepareFailView(String error);
 }
--- src/use_case/send_request/SendRequestUserDataAccessInterface.java
+++ src/use_case/send_request/SendRequestUserDataAccessInterface.java
@@ -5,18 +5,17 @@ import entity.User;
 public interface SendRequestUserDataAccessInterface {
 
     /**
-     * Checks for the sender's username in the receiver's list of friend requests, and returns
-     * whether the sender has previously requested the receiver.
+     * Returns whether the sender has previously requested the receiver.
      * @param sender
      * @param receiver
-     * @return true if previously requested (false otherwise)
+     * @return True if previously requested (False otherwise)
      */
     boolean isRequested(User sender, User receiver);
 
     /**
      * Sends a friend request by adding the sender's username into the receiver's list of friend requests.
-     * @param sender
-     * @param receiver
+     * @param sender The user who is sending the friend request
+     * @param receiver The user who is receiving the friend request
      */
     void sendFriendRequest(User sender, User receiver);
     User get(String username);
