b730e9685f19f904143aa614a9d5b308b02d5f03
Ryan <ryanning15243@gmail.com>
2023-12-03 11:56:41 -0500

    Implemented prepareSuccessView method for AcceptRequestPresenter Class

--- src/interface_adapter/accept_request/AcceptRequestPresenter.java
+++ src/interface_adapter/accept_request/AcceptRequestPresenter.java
@@ -1,9 +1,19 @@
 package interface_adapter.accept_request;
 
-public class AcceptRequestPresenter {
+import use_case.accept_request.AcceptRequestOutputBoundary;
+import use_case.accept_request.AcceptRequestOutputData;
+
+public class AcceptRequestPresenter implements AcceptRequestOutputBoundary {
     private AcceptRequestViewModel viewModel;
     public AcceptRequestPresenter(AcceptRequestViewModel viewModel) {
         this.viewModel = viewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(AcceptRequestOutputData acceptRequestOutputData) {
+        AcceptRequestState newState = new AcceptRequestState();
+        newState.setRequests(acceptRequestOutputData.getRequests());
+        viewModel.firePropertyChanged();
     }
 
 }
