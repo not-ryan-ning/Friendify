6d4e6fbdab84f188ff7c97678ad5776724acb880
Olivia <olivia.w11267@gmail.com>
2023-12-02 19:33:31 -0500

    Implemented send request interactor test

--- test/use_case/send_request/SendRequestInteractorTest.java
+++ test/use_case/send_request/SendRequestInteractorTest.java
@@ -1,9 +1,18 @@
 package use_case.send_request;
 
+import data_access.FileUserDataAccessObject;
+import entity.CommonProfileFactory;
+import entity.CommonUserFactory;
+import entity.ProfileFactory;
+import entity.UserFactory;
 import interface_adapter.ViewManagerModel;
+import interface_adapter.send_request.SendRequestPresenter;
+import interface_adapter.send_request.SendRequestViewModel;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import use_case.match.MatchInteractor;
+
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
@@ -12,10 +21,13 @@ public class SendRequestInteractorTest {
     private ViewManagerModel viewManagerModel;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws IOException {
         // Initialize the objects before each test
         viewManagerModel = new ViewManagerModel();
-        sendRequestInteractor = ...
+        UserFactory uf = new CommonUserFactory();
+        ProfileFactory pf = new CommonProfileFactory();
+        SendRequestUserDataAccessInterface user = new FileUserDataAccessObject("./users", uf, pf);
+        sendRequestInteractor = new SendRequestInteractor(user, new SendRequestPresenter(new SendRequestViewModel(), viewManagerModel));
     }
     @Test
     public void testExecute() {
