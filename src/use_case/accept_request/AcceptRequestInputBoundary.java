6b4d0c92399b0ed0b66e3be33b73a9f7a8699b38
Ryan <ryanning15243@gmail.com>
2023-12-03 11:45:50 -0500

    Implemented empty methods in AcceptRequestInputBoundary class

--- src/use_case/accept_request/AcceptRequestInputBoundary.java
+++ src/use_case/accept_request/AcceptRequestInputBoundary.java
@@ -1,4 +1,9 @@
 package use_case.accept_request;
 
+import entity.User;
+
 public interface AcceptRequestInputBoundary {
+    void updateUserInformation(User user);
+
+    User get(String currentUsername);
 }
