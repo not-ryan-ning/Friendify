1c059ba119e9b98ad9069b99daddc17e708e6dae
Ryan <ryanning15243@gmail.com>
2023-12-03 11:57:32 -0500

    Implemented initializers for AcceptRequestState Class

--- src/interface_adapter/accept_request/AcceptRequestState.java
+++ src/interface_adapter/accept_request/AcceptRequestState.java
@@ -1,4 +1,14 @@
 package interface_adapter.accept_request;
 
+import java.util.ArrayList;
+
 public class AcceptRequestState {
+    private ArrayList<String> requests;
+    private String username;
+    public AcceptRequestState(AcceptRequestState copy) {
+        this.requests = copy.requests;
+        this.username = copy.username;
+    }
+    public AcceptRequestState() {
+    }
 }
