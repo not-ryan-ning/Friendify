a049b8a43df311bf67ee24f05eb38dbe4e034a9d
Ryan <ryanning15243@gmail.com>
2023-12-04 09:55:19 -0500

    Implemented initializers for GenreStrategyTest

---
+++ test/entity/GenreStrategyTest.java
@@ -0,0 +1,29 @@
+package entity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Testing file for GenreStrategy Class
+ */
+public class GenreStrategyTest {
+    private PlaylistFactory factory;
+    private MatchingStrategy matchingStrategy;
+
+    /**
+     * Initialize Playlists with empty ID int and titles, artists, and genres ArrayLists
+     * Predetermine certain parameters to see if Playlists return the correct similarity score
+     */
+    @Before
+    public void init() {
+        this.factory = new CommonPlaylistFactory();
+        this.matchingStrategy = new GenreStrategy();
+    }
+}
