7d165fad026b36f0f21b46f9213fbf0730955b0e
Ryan <ryanning15243@gmail.com>
2023-12-04 18:29:44 -0500

    Minor: Fixed naming of file

--- test/use_case/accept_request/AcceptRequestInteractorTest.java
+++ test/use_case/accept_request/AcceptRequestInteractorTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class AcceptRequestUseCaseTest {
+public class AcceptRequestInteractorTest {
     private UserFactory userFactory;
     private ProfileFactory profileFactory;
     private PlaylistFactory playlistFactory;
